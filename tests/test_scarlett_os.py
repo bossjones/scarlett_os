18c4fc586aaaf138bb301d2900a3df7d1c5af309
Malcolm Jones <malcolm@adobe.com>
2016-09-21 09:05:13 -0400

    fix: testing works?

--- tests/test_scarlett_os.py
+++ tests/test_scarlett_os.py
@@ -39,7 +39,7 @@ class TestScarlett_os(unittest.TestCase):
         Gst.debug_set_active(True)
         Gst.debug_set_default_threshold(1)
 
-        assert GObject.pygobject_version == (3, 20, 0)
+        assert GObject.pygobject_version == (3, 21, 92)
         assert Gst.version_string() == 'GStreamer 1.8.2'
 
     def test_command_line_interface(self):
