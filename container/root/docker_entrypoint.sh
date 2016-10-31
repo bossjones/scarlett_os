c42ec7698cafd7d8f57d1d9f1d3b11f3fa5c03c5
Malcolm Jones <malcolm@adobe.com>
2016-10-31 14:01:38 -0400

    chg: add chown

--- container/root/docker_entrypoint.sh
+++ container/root/docker_entrypoint.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-# if [ ! -f /virtualenv/bin/activate ]; then
-#     virtualenv /virtualenv -p python${INSPIRE_PYTHON_VERSION}
-#     source /virtualenv/bin/activate
-#     pip install --upgrade pip setuptools wheel
-#     cp -r /src-cache /virtualenv/src
-# else
-#     source /virtualenv/bin/activate
-# fi
-
 sudo find \( -name __pycache__ -o -name '*.pyc' \) | sudo xargs rm -rf
 sudo chown pi:pi -R /home/pi/dev
 
