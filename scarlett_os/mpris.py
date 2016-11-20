411ba6ed1955d753bd72f6aafededcc85c6d6f18
Malcolm Jones <malcolm@adobe.com>
2016-11-20 13:01:59 -0500

    Chg: fixed utf-8 encoding issues from copy paste

--- scarlett_os/mpris.py
+++ scarlett_os/mpris.py
@@ -5,11 +5,11 @@
 
 # __future__ is a real module, and serves three purposes:
 #
-# To avoid confusing existing tools that analyze import statements and expect to find the modules they’re importing.
+# To avoid confusing existing tools that analyze import statements and expect to find the modules theyre importing.
 #
 # To ensure that future statements run under releases prior to 2.1 at least yield runtime exceptions (the import of __future__ will fail, because there was no module of that name prior to 2.1).
 #
-# To document when incompatible changes were introduced, and when they will be — or were — made mandatory. This is a form of executable documentation, and can be inspected programmatically via importing __future__ and examining its contents.
+# To document when incompatible changes were introduced, and when they will be  or were  made mandatory. This is a form of executable documentation, and can be inspected programmatically via importing __future__ and examining its contents.
 
 # with:
 #    This PEP adds a new statement "with" to the Python language to make it possible to
