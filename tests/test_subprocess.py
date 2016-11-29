2c551e48dd84fdb302a49152840e63c3f9bb575f
Malcolm Jones <malcolm@adobe.com>
2016-11-28 20:29:56 -0500

    Chg: patched scarlett_os.subprocess.Subprocess.__init__

--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -15,8 +15,10 @@ import unittest
 import unittest.mock as mock
 
 import scarlett_os
-from scarlett_os.subprocess import check_pid
-from scarlett_os.internal.gi import GLib, GObject
+from scarlett_os.subprocess import check_pid, Subprocess
+
+# NOTE: We can't add this here, otherwise we won't be able to mock them
+# from scarlett_os.internal.gi import GLib, GObject
 
 from tests import common
 
@@ -32,10 +34,16 @@ def raise_OSError(*x, **kw):
     raise OSError('Fail')
 
 
-class MockSubprocess(GObject.GObject):
+# class MockSubprocess(GObject.GObject):
+#
+#     def __init__(self):
+#         GObject.GObject.__init__(self, common.create_scarlett_os_subprocess_mock())
+
+
+class SubprocessStub(object):
 
     def __init__(self):
-        GObject.GObject.__init__(self, common.create_scarlett_os_subprocess_mock())
+        super(self).__init__()
 
 
 # class MockedPipeline(pipeline.Pipeline):
@@ -71,87 +79,162 @@ class TestScarlettSubprocess(unittest.TestCase):
 
     #    self.register_server_socket(self.server_socket.fileno())
 
-    # NOTE: Decorators get applied BOTTOM to TOP
-    @mock.patch("scarlett_os.internal.gi.GLib.spawn_async")
-    def test_check_command_type_not_str(self, mock_glib_spawn_async):
-        # test_pid, test_stdin, test_stdout, test_stderr = GLib.spawn_async(test_command,
-        #                                                                   flags=GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD
-        #                                                                   )
-        #
-        # Out[11]: (23241, None, None, None)
-        #
-        # In [12]:  07:29:39 up 20:10,  3 users,  load average: 0.11, 0.08, 0.02
-        # USER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT
-        # In [12]:
-        #
-        # In [12]:
-
-        test_result = '''
-pi       tty7         2016-11-24 11:19 (:0)
-pi       pts/5        2016-11-24 11:20 (10.0.2.2)
-pi       pts/17       2016-11-24 11:20 (10.0.2.2)
-'''
-        # NOTE: On purpose this is an invalid cmd. Should be of type array
-        test_command = 'who'
-        test_name = None
-        test_fork = False
-        test_return_tuple = (23241, None, None, None)
-
-        # command, name=None, fork=False
-        s_test = scarlett_os.subprocess.Subprocess(test_command, test_name, test_fork)
-
-        # action
-        s_test.spawn_command()
-
-        # assert
-        mock_glib_spawn_async.assert_called_once_with(test_command,
-                                                      flags=GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD)
-
-        pass
+    # @mock.patch('myapp.app.Car')
+    # def test_class(self, mock_car):
+    #
+    #     class NewCar(object):
+    #
+    #         def get_make(self):
+    #             return 'Audi'
+    #
+    #         @property
+    #         def wheels(self):
+    #             return 6
+    #
+    #     mock_car.return_value = NewCar()
+    #     self.assertEquals(get_car_make(), 'Audi')
+    #     self.assertEquals(get_car_wheels(), 6)
 
     # NOTE: Decorators get applied BOTTOM to TOP
-    @mock.patch("scarlett_os.internal.gi.GLib.spawn_async")
-    @mock.patch("scarlett_os.subprocess.Subprocess.check_command_type")
-    def test_spawn_command(self, mock_glib_spawn_async, mock_check_command_type):
-        # test_pid, test_stdin, test_stdout, test_stderr = GLib.spawn_async(test_command,
-        #                                                                   flags=GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD
-        #                                                                   )
-        #
-        # Out[11]: (23241, None, None, None)
-        #
-        # In [12]:  07:29:39 up 20:10,  3 users,  load average: 0.11, 0.08, 0.02
-        # USER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT
-        # In [12]:
-        #
-        # In [12]:
+    # @mock.patch("scarlett_os.internal.gi.GLib.spawn_async")
+    # @mock.patch('scarlett_os.subprocess.Subprocess')
+    def test_check_command_type_is_array_of_str(self):
+        # source: http://stackoverflow.com/questions/28181867/how-do-a-mock-a-superclass-that-is-part-of-a-library
+        self.assertRaises(Exception, Subprocess)  # Normal implementation raise Exception
 
-        test_result = '''
-pi       tty7         2016-11-24 11:19 (:0)
-pi       pts/5        2016-11-24 11:20 (10.0.2.2)
-pi       pts/17       2016-11-24 11:20 (10.0.2.2)
-'''
-        test_command = ['who']
-        test_name = None
-        test_fork = False
-        test_return_tuple = (23241, None, None, None)
+        # Pay attention to return_value MUST be None for all __init__ methods
+        with mock.patch("scarlett_os.subprocess.Subprocess.__init__", autospec=True, return_value=None) as mock_init:
+            self.assertRaises(TypeError, Subprocess)  # Wrong argument: autospec=True let as to catch it
+            s = Subprocess(['who'])  # Ok now it works
+            mock_init.assert_called_with(mock.ANY, ['who'])  # Use autospec=True inject self as first argument -> use Any to discard it
+            # But launch_nukes() was still the original one and it will raise
+            # self.assertRaises(Exception, s.party)
+            # pass
+#         #
+#         # class NewSubprocess(object):
+#         #
+#         # test_pid, test_stdin, test_stdout, test_stderr = GLib.spawn_async(test_command,
+#         #                                                                   flags=GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD
+#         #                                                                   )
+#         #
+#         # Out[11]: (23241, None, None, None)
+#         #
+#         # In [12]:  07:29:39 up 20:10,  3 users,  load average: 0.11, 0.08, 0.02
+#         # USER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT
+#         # In [12]:
+#         #
+#         # In [12]:
+#
+#         test_result = '''
+# pi       tty7         2016-11-24 11:19 (:0)
+# pi       pts/5        2016-11-24 11:20 (10.0.2.2)
+# pi       pts/17       2016-11-24 11:20 (10.0.2.2)
+# '''
+#         # NOTE: On purpose this is an invalid cmd. Should be of type array
+#         test_command = ['who']
+#
+#         test_name = None
+#         test_fork = False
+#         # test_return_tuple = (23241, None, None, None)
+#
+#         # command, name=None, fork=False
+#         s_test = scarlett_os.subprocess.Subprocess(test_command, test_name, test_fork)
+#
+#         # action
+#         self.assertEqual(s_test.check_command_type(test_command), True)
+#         #
+#         # # assert
+#         # mock_glib_spawn_async.assert_called_once_with(test_command,
+#         #                                               flags=GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD)
+#         #
+#         # pass
 
-        # mock
-        mock_check_command_type.return_value = True
 
-        # action
-        # command, name=None, fork=False
-        s_test = scarlett_os.subprocess.Subprocess(test_command, test_name, test_fork)
-        s_test.spawn_command()
 
-        # assert
-        self.assertEquals(s_test.check_command_type(test_command), True)
-        self.assertEquals(mock_glib_spawn_async.call_count, 1)
 
-        # scarlett_os.subprocess.Subprocess.__init__(
-        #     self.mock, mock.sentinel.command)
-        # self.mock.create_server_socket.assert_called_once_with(
-        #     mock.sentinel.host, mock.sentinel.port)
-        pass
+
+#
+#     # NOTE: Decorators get applied BOTTOM to TOP
+#     @mock.patch("scarlett_os.internal.gi.GLib.spawn_async")
+#     def test_check_command_type_not_str(self, mock_glib_spawn_async):
+#         # test_pid, test_stdin, test_stdout, test_stderr = GLib.spawn_async(test_command,
+#         #                                                                   flags=GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD
+#         #                                                                   )
+#         #
+#         # Out[11]: (23241, None, None, None)
+#         #
+#         # In [12]:  07:29:39 up 20:10,  3 users,  load average: 0.11, 0.08, 0.02
+#         # USER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT
+#         # In [12]:
+#         #
+#         # In [12]:
+#
+#         test_result = '''
+# pi       tty7         2016-11-24 11:19 (:0)
+# pi       pts/5        2016-11-24 11:20 (10.0.2.2)
+# pi       pts/17       2016-11-24 11:20 (10.0.2.2)
+# '''
+#         # NOTE: On purpose this is an invalid cmd. Should be of type array
+#         test_command = 'who'
+#         test_name = None
+#         test_fork = False
+#         test_return_tuple = (23241, None, None, None)
+#
+#         # command, name=None, fork=False
+#         s_test = scarlett_os.subprocess.Subprocess(test_command, test_name, test_fork)
+#
+#         # action
+#         s_test.spawn_command()
+#
+#         # assert
+#         mock_glib_spawn_async.assert_called_once_with(test_command,
+#                                                       flags=GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD)
+#
+#         pass
+#
+#     # NOTE: Decorators get applied BOTTOM to TOP
+#     @mock.patch("scarlett_os.internal.gi.GLib.spawn_async")
+#     @mock.patch("scarlett_os.subprocess.Subprocess.check_command_type")
+#     def test_spawn_command(self, mock_glib_spawn_async, mock_check_command_type):
+#         # test_pid, test_stdin, test_stdout, test_stderr = GLib.spawn_async(test_command,
+#         #                                                                   flags=GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD
+#         #                                                                   )
+#         #
+#         # Out[11]: (23241, None, None, None)
+#         #
+#         # In [12]:  07:29:39 up 20:10,  3 users,  load average: 0.11, 0.08, 0.02
+#         # USER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT
+#         # In [12]:
+#         #
+#         # In [12]:
+#
+#         test_result = '''
+# pi       tty7         2016-11-24 11:19 (:0)
+# pi       pts/5        2016-11-24 11:20 (10.0.2.2)
+# pi       pts/17       2016-11-24 11:20 (10.0.2.2)
+# '''
+#         test_command = ['who']
+#         test_name = None
+#         test_fork = False
+#         test_return_tuple = (23241, None, None, None)
+#
+#         # mock
+#         mock_check_command_type.return_value = True
+#
+#         # action
+#         # command, name=None, fork=False
+#         s_test = scarlett_os.subprocess.Subprocess(test_command, test_name, test_fork)
+#         s_test.spawn_command()
+#
+#         # assert
+#         self.assertEquals(s_test.check_command_type(test_command), True)
+#         self.assertEquals(mock_glib_spawn_async.call_count, 1)
+#
+#         # scarlett_os.subprocess.Subprocess.__init__(
+#         #     self.mock, mock.sentinel.command)
+#         # self.mock.create_server_socket.assert_called_once_with(
+#         #     mock.sentinel.host, mock.sentinel.port)
+#         pass
 
 
 # In [2]: import os
