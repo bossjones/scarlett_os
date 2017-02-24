ffd7872a4aae15a56bc8ff7260edefb9347e6e28
Malcolm Jones <malcolm@adobe.com>
2017-02-24 21:00:52 +0100

    WIP: logs are NOT THREADSAFE CURRENTLY B

--- scarlett_os/tasker.py
+++ scarlett_os/tasker.py
@@ -48,6 +48,7 @@ from scarlett_os.utility.generators import Queue
 
 #######################################
 # FIXME: Temporary, till we figure out best way to approach logging module
+# FIXME: FAM, THIS SHIT IS NOT THREADSAFE AT ALL. LETS SEE HOW MOPIDY/PITIVI/SOMEBODY-ELSE IS HANDLING THINGS
 # import scarlett_os.logger
 import logging
 import logging.config
@@ -55,41 +56,41 @@ import logging.handlers
 
 logger = logging.getLogger(__name__)
 
-try:
-    from colorlog import ColoredFormatter
-    from gettext import gettext as _  # noqa
-    """Return a logger with a default ColoredFormatter."""
-    formatter = ColoredFormatter(
-        "(%(threadName)-9s) %(log_color)s%(levelname)-8s%(reset)s (%(funcName)-5s) %(message_log_color)s%(message)s",  # noqa
-        datefmt=None,
-        reset=True,
-        log_colors={
-            'DEBUG': 'cyan',
-            'INFO': 'green',
-            'WARNING': 'yellow',
-            'ERROR': 'red',
-            'CRITICAL': 'bold_red',
-            'TRACE': 'purple'
-        },
-        secondary_log_colors={
-            'message': {
-                'ERROR': 'red',
-                'CRITICAL': 'red',
-                'DEBUG': 'yellow',
-                'INFO': 'yellow,bg_blue'
-            }
-        },
-        style='%'
-    )
-
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(logging.DEBUG)
-except ImportError:
-    # No color available, use default config
-    logging.basicConfig(format='%(levelname)s: %(message)s')
-    logger.warn("Disabling color, you really want to install colorlog.")
+# try:
+#     from colorlog import ColoredFormatter
+#     from gettext import gettext as _  # noqa
+#     """Return a logger with a default ColoredFormatter."""
+#     formatter = ColoredFormatter(
+#         "(%(threadName)-9s) %(log_color)s%(levelname)-8s%(reset)s (%(funcName)-5s) %(message_log_color)s%(message)s",  # noqa
+#         datefmt=None,
+#         reset=True,
+#         log_colors={
+#             'DEBUG': 'cyan',
+#             'INFO': 'green',
+#             'WARNING': 'yellow',
+#             'ERROR': 'red',
+#             'CRITICAL': 'bold_red',
+#             'TRACE': 'purple'
+#         },
+#         secondary_log_colors={
+#             'message': {
+#                 'ERROR': 'red',
+#                 'CRITICAL': 'red',
+#                 'DEBUG': 'yellow',
+#                 'INFO': 'yellow,bg_blue'
+#             }
+#         },
+#         style='%'
+#     )
+#
+#     handler = logging.StreamHandler()
+#     handler.setFormatter(formatter)
+#     logger.addHandler(handler)
+#     logger.setLevel(logging.DEBUG)
+# except ImportError:
+#     # No color available, use default config
+#     logging.basicConfig(format='%(levelname)s: %(message)s')
+#     logger.warn("Disabling color, you really want to install colorlog.")
 ########################################
 
 # global pretty print for debugging
