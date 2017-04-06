effb9c055f687a2c04d4b110eb7a5e342ce740ae
Malcolm Jones <malcolm@adobe.com>
2017-04-05 21:19:02 -0400

    chg: test_mpris still broken, need it to be in full pytest format

--- tests/test_mpris.py
+++ tests/test_mpris.py
@@ -8,60 +8,80 @@ test_mpris
 
 import os
 import sys
-import unittest
+import pytest
+# import unittest
 
 from scarlett_os.internal.gi import GLib
 from scarlett_os.internal.gi import Gio
 from scarlett_os.internal.gi import GObject
 
-from pydbus import SessionBus
-from scarlett_os.mpris import ScarlettListener
-
-bus = SessionBus()
-bus.own_name(name='org.scarlett')
-
-
 DBUS_SESSION_BUS_ADDRESS = os.getenv("DBUS_SESSION_BUS_ADDRESS")
 
+@pytest.fixture(scope='module')
+def bus():
+    from pydbus import SessionBus
+    bus = SessionBus()
+    print('\n[initalize] pydbus.SessionBus ...')
+    bus.own_name(name='org.scarlett')
+    bus.dbus
+    yield bus
+    print('\n[teardown] pydbus.SessionBus ...')
+    del bus._dbus
+    print("ran: del bus._dbus")
+    del bus
+    print("ran: del bus")
 
-class TestScarlettListener(unittest.TestCase):
+@pytest.fixture(scope='module')
+@pytest.mark.unittest
+# @pytest.mark.unittest
+def sl(bus):
+    from scarlett_os.mpris import ScarlettListener
+    sl = ScarlettListener(bus=bus.con, path='/org/scarlett/Listener')
+    print('\n[initalize] scarlett_os.mpris.ScarlettListener ...')
+    yield sl
+    print('\n[teardown] scarlett_os.mpris.ScarlettListener ...')
+    del sl
+    print("ran: del sl")
 
-    def setUp(self):
-        """
-        Method called to prepare the test fixture. This is called immediately before calling the test method; other than AssertionError or SkipTest, any exception raised by this method will be considered an error rather than a test failure. The default implementation does nothing.
-        """
-        self.sl = ScarlettListener(bus=bus.con, path='/org/scarlett/Listener')
+
+class TestScarlettListener(object):
+
+    # def setUp(self):
+    #     """
+    #     Method called to prepare the test fixture. This is called immediately before calling the test method; other than AssertionError or SkipTest, any exception raised by this method will be considered an error rather than a test failure. The default implementation does nothing.
+    #     """
+    #     self.sl = ScarlettListener(bus=bus.con, path='/org/scarlett/Listener')
 
     # NOTE: Added 1/4/2017
     # def tearDown(self):
     #     del self.sl
 
-    def test_scarlett_listener_interfaces(self):
-        self.assertEqual(self.sl.__repr__(), '<ScarlettListener(org.scarlett, /org/scarlett/Listener)>')
-        self.assertEqual(self.sl.LISTENER_IFACE, 'org.scarlett.Listener')
-        self.assertEqual(self.sl.LISTENER_PLAYER_IFACE, 'org.scarlett.Listener.Player')
-        self.assertEqual(self.sl.LISTENER_TRACKLIST_IFACE, 'org.scarlett.Listener.TrackList')
-        self.assertEqual(self.sl.LISTENER_PLAYLISTS_IFACE, 'org.scarlett.Listener.Playlists')
-        self.assertEqual(self.sl.LISTENER_EVENTS_IFACE, 'org.scarlett.Listener.event')
-        self.assertTrue(isinstance(self.sl.bus_conn, Gio.DBusConnection))
-        self.assertEqual(self.sl.path, '/org/scarlett/Listener')
+    def test_scarlett_listener_interfaces(self, sl):
+        assert sl.__repr__() == '<ScarlettListener(org.scarlett, /org/scarlett/Listener)>'
+        assert sl.LISTENER_IFACE == 'org.scarlett.Listener'
+        assert sl.LISTENER_PLAYER_IFACE == 'org.scarlett.Listener.Player'
+        assert sl.LISTENER_TRACKLIST_IFACE == 'org.scarlett.Listener.TrackList'
+        assert sl.LISTENER_PLAYLISTS_IFACE == 'org.scarlett.Listener.Playlists'
+        assert sl.LISTENER_EVENTS_IFACE == 'org.scarlett.Listener.event'
+        assert isinstance(sl.bus_conn, Gio.DBusConnection)
+        assert sl.path == '/org/scarlett/Listener'
 
         # NOTE: these are all private. We need a getter function
-        # self.assertTrue(type(self.sl.dbus_stack), list)
-        # self.assertEqual(self.sl._message, 'This is the DBusServer')
-        # self.assertEqual(self.sl._status_ready, '  ScarlettListener is ready')
-        # self.assertEqual(self.sl._status_kw_match, "  ScarlettListener caught a keyword match")
-        # self.assertEqual(self.sl._status_cmd_match, "  ScarlettListener caught a command match")
-        # self.assertEqual(self.sl._status_stt_failed, "  ScarlettListener hit Max STT failures")
-        # self.assertEqual(self.sl._status_cmd_start, "  ScarlettListener emitting start command")
-        # self.assertEqual(self.sl._status_cmd_fin, "  ScarlettListener Emitting Command run finish")
-        # self.assertEqual(self.sl._status_cmd_cancel, "  ScarlettListener cancel speech Recognition")
+        # self.assertTrue(type(sl.dbus_stack), list)
+        # self.assertEqual(sl._message, 'This is the DBusServer')
+        # self.assertEqual(sl._status_ready, '  ScarlettListener is ready')
+        # self.assertEqual(sl._status_kw_match, "  ScarlettListener caught a keyword match")
+        # self.assertEqual(sl._status_cmd_match, "  ScarlettListener caught a command match")
+        # self.assertEqual(sl._status_stt_failed, "  ScarlettListener hit Max STT failures")
+        # self.assertEqual(sl._status_cmd_start, "  ScarlettListener emitting start command")
+        # self.assertEqual(sl._status_cmd_fin, "  ScarlettListener Emitting Command run finish")
+        # self.assertEqual(sl._status_cmd_cancel, "  ScarlettListener cancel speech Recognition")
 
     def test_scarlett_listener_emit_methods(self):
-        self.assertEqual(self.sl.emitKeywordRecognizedSignal(), 'pi-listening')
-        self.assertEqual(self.sl.emitCommandRecognizedSignal('what time is it'), 'pi-response')
-        self.assertEqual(self.sl.emitSttFailedSignal(), 'pi-response2')  # SCARLETT_FAILED
-        self.assertEqual(self.sl.emitListenerCancelSignal(), 'pi-cancel')  # SCARLETT_CANCEL
-        self.assertEqual(self.sl.emitListenerReadySignal(), 'pi-listening')  # SCARLETT_LISTENING
-        self.assertEqual(self.sl.emitConnectedToListener('fake_plugin'), ' fake_plugin is connected to ScarlettListener')
-        self.assertEqual(self.sl.emitListenerMessage(), 'This is the DBusServer')
+        assert sl.emitKeywordRecognizedSignal() == 'pi-listening'
+        assert sl.emitCommandRecognizedSignal('what time is it') == 'pi-response'
+        assert sl.emitSttFailedSignal() == 'pi-response2'  # SCARLETT_FAILED
+        assert sl.emitListenerCancelSignal() == 'pi-cancel'  # SCARLETT_CANCEL
+        assert sl.emitListenerReadySignal() == 'pi-listening'  # SCARLETT_LISTENING
+        assert sl.emitConnectedToListener('fake_plugin') == ' fake_plugin is connected to ScarlettListener'
+        assert sl.emitListenerMessage() == 'This is the DBusServer'
