1d81c2afaa16de3e08682aba19dc4fa1596c6358
Malcolm Jones <malcolm@adobe.com>
2017-08-26 14:42:47 -0400

    Chg: added a bit of info for configure script

--- scripts/configure_pipconf_to_use_devpi.sh
+++ scripts/configure_pipconf_to_use_devpi.sh
@@ -9,3 +9,11 @@ export HOST_IP=$(ip route| awk '/^default/ {print $3}') \
 && echo [install] >> ~/.pip/pip.conf \
 && echo trusted-host = $HOST_IP >> ~/.pip/pip.conf \
 && cat ~/.pip/pip.conf
+
+
+echo "if it's working you will see out put including:"
+echo "2 location(s) to search for versions of ignoredd:"
+echo "* https://pypi.python.org/simple/ignoredd/"
+echo "* http://172.17.0.1:3141/app/dev/+simple/ignoredd/"
+
+pip install -v ignoredd
