c66568960a2bf37e0a959be7c22edbc70cd57d6c
Malcolm Jones <malcolm@adobe.com>
2018-04-15 22:39:37 -0400

    wip: makefile

--- flatpak_build.sh
+++ flatpak_build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 pyenv init - > ~/pyenv_temp
