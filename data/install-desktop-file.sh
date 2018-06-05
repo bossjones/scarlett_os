da2a8edb0db7122ee152b861a8f64a10dc714da0
Malcolm Jones <malcolm@adobe.com>
2018-06-04 21:37:37 -0400

    WIP: hacking-env

---
+++ data/install-desktop-file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# desktop-file-edit --set-key Name --set-value 'ScarlettOS' --set-key Exec --set-value 'who' --set-key Icon --set-value org.scarlett.ScarlettOS scarlettos.desktop
+
+install -D org.scarlett.ScarlettOS.desktop /app/share/applications/
